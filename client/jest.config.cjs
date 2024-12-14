539ee448ca8d343cdc42cc0be2847edaa6a03dec
Brandon <fortuneblue25@gmail.com>
2024-12-13 19:37:26 -0600

    u

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -1,16 +1,3 @@
-// module.exports = {
-//   testEnvironment: 'jest-environment-jsdom', // Simulate a browser environment
-//   transform: {
-//     '^.+\\.(js|jsx)$': 'babel-jest', // Use Babel for .js and .jsx files
-//   },
-//   moduleNameMapper: {
-//     '\\.(css|scss)$': 'identity-obj-proxy', // Mock CSS/SCSS imports
-//     '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js', // Mock image imports
-//   },
-//   setupFilesAfterEnv: ['@testing-library/jest-dom'], // Add extra matchers for assertions
-//   testPathIgnorePatterns: ['/node_modules/', '/dist/'], // Ignore unnecessary folders
-// };
-
 module.exports = {
   testEnvironment: 'jest-environment-jsdom', // Simulate a browser environment
   transform: {
